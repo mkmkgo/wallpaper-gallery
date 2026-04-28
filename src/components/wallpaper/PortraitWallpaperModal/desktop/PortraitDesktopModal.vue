079b350227ba7b2c9130e2e1b169cfc404745dec
Mkboy <1109008002@qq.com>
2026-04-28 09:18:09 +0800

    恢复真机预览头像图片

--- src/components/wallpaper/PortraitWallpaperModal/desktop/PortraitDesktopModal.vue
+++ src/components/wallpaper/PortraitWallpaperModal/desktop/PortraitDesktopModal.vue
@@ -711,7 +711,7 @@ onUnmounted(() => {
     height: 0;
     border-radius: 50%;
     // background: var(--accent-gradient);
-    background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+    background: url('https://photos5.appleinsider.com/gallery/50657-99800-Tim-Cook-xl.jpg') center/cover;
     margin-right: 8px;
     transition: 0.4s;
   }
