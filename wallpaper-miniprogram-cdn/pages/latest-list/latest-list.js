ae9e8d8d5a2fadc00aa41dc0c833a2a469e5f2df
mkboy95 <1109008002@qq.com>
2026-05-23 02:14:47 +0800

    fix: 优先用filename中的日期判断，修复UTC时区导致5.22壁纸归到5.21

--- wallpaper-miniprogram-cdn/pages/latest-list/latest-list.js
+++ wallpaper-miniprogram-cdn/pages/latest-list/latest-list.js
@@ -173,14 +173,14 @@ Page({
   },
 
   extractDateFromWallpaper: function(wp) {
-    if (wp.createdAt) {
-      var d = wp.createdAt.split("T")[0];
-      if (d) return d;
-    }
     var fname = wp.filename || wp.id || "";
     var match = fname.match(/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/);
     if (match) {
       return match[1] + "-" + match[2] + "-" + match[3];
+    }
+    if (wp.createdAt) {
+      var d = wp.createdAt.split("T")[0];
+      if (d) return d;
     }
     if (wp.category) {
       return wp.category;
